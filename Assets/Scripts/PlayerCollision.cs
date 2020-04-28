a205059c17d006e8c4030087758503c03f2319da
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-04-28 19:38:10 +0530

    obstacle

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,9 @@ public class PlayerCollision : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
     }
 }

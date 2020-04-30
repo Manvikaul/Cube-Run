edff386ec1aea41b061dd040fdf05afbdd0ccf14
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-04-30 21:16:25 +0530

    game end

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    bool gameEnded = false;
+    public float delay=1f;
 
     public void EndGame()
     {
-        Debug.Log("GAME OVER!");
+        if(gameEnded==false)
+        {
+            gameEnded = true;
+            Debug.Log("GAME OVER!");
+            Invoke("Restart", delay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }

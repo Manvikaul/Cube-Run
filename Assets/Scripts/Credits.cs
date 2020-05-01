28105bb16dfa704438538e8483c16133f943637b
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-05-01 23:59:44 +0530

    edits

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void quit()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Quit!");
+        Application.Quit();
     }
 }
